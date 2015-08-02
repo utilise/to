9905bf57959b80c8d27f2191a6f9429bac5d0410
pemrouz <p3pedram@hotmail.com>
2015-08-02 20:10:18 +0100

    add to.obj

--- test.js
+++ test.js
@@ -8,4 +8,69 @@ describe('to', function() {
     expect(arguments.unshift).to.not.be.ok
   })
 
+  it('should use default id prop if none specified', function() {
+    var a = [ 
+      { 
+        'id': 'apples'
+      , 'color': 'green' 
+      }
+    , { 
+        'id': 'bananas'
+      , 'color': 'yellow' 
+      }
+    ]
+
+    expect(a.reduce(to.obj, 1)).to.eql({
+      apples: { 
+        'id': 'apples'
+      , 'color': 'green' 
+      }
+    , bananas: { 
+        'id': 'bananas'
+      , 'color': 'yellow' 
+      }
+    })
+  })
+
+
+  it('should work with array of size 1', function() {
+    var a = [ 
+      { 
+        'id': 'apples'
+      , 'color': 'green' 
+      }
+    ]
+
+    expect(a.reduce(to.obj, 1)).to.eql({
+      apples: { 
+        'id': 'apples'
+      , 'color': 'green' 
+      }
+    })
+  })
+
+  it('should convert array to object', function() {
+    var a = [ 
+      { 
+        'name': 'apples'
+      , 'color': 'green' 
+      }
+    , { 
+        'name': 'bananas'
+      , 'color': 'yellow' 
+      }
+    ]
+
+    expect(a.reduce(to.obj('name'), 1)).to.eql({
+      apples: { 
+        'name': 'apples'
+      , 'color': 'green' 
+      }
+    , bananas: { 
+        'name': 'bananas'
+      , 'color': 'yellow' 
+      }
+    })
+  })
+
 })
